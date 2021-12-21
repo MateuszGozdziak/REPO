8ba44ad8183af7f20b3c6793f511422c981941ba
Konrad Dzięcioł <konradd990212@gmail.com>
2021-12-21 21:38:10 +0100

    aaa

---
+++ Page_Klienci_Dodaj.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EfficientBook
+{
+    /// <summary>
+    /// Interaction logic for Page_Klienci_Dodaj.xaml
+    /// </summary>
+    public partial class Page_Klienci_Dodaj : Page
+    {
+        public Page_Klienci_Dodaj()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Page_Klienci_dodaj_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
