ec6fb154b390b18fb66aa040cdfb1cb51fb7f547
Konrad Dzięcioł <konradd990212@gmail.com>
2021-12-21 17:09:27 +0100

    neccesery

--- DataBaseConnections.cs
+++ DataBaseConnections.cs
@@ -1,9 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySqlConnector;
 using System.Windows;
 
 namespace EfficientBook
